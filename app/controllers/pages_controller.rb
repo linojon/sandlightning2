3a7598e6952004f399b9b1d9eb518ad61cb76ca0
Jonathan Linowes <jonathan@parkerhill.com>
2016-07-12 18:41:15 -0400

    use scope for forum order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def home
-    @forums = Forum.order("created_at DESC").all
+    @forums = Forum.all
   end
 
 end
--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,3 +1,4 @@
 class Forum < ApplicationRecord
   has_many :talks, dependent: :destroy
+  default_scope { order("created_at DESC") }
 end
